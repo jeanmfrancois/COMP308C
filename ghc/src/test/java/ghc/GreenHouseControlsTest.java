ea9926a3635b2fe8bd02e045f242dcd8e1ba94b5
Jean-francois Nepton <jeanmfrancois@hotmail.com>
2015-01-08 02:35:40 -0500

    Create Initial Structure

---
+++ ghc/src/test/java/ghc/GreenHouseControlsTest.java
@@ -0,0 +1,15 @@
+package ghc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GreenHouseControlsTest {
+
+	@Test
+	public void test() {
+		System.out.println("Performing Test");
+		//fail("Not yet implemented");
+	}
+
+}
